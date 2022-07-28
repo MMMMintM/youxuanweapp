c3c7854823d1cf78e09c92587dea4e6958c80e54
MMMMintM <1962849542@qq.com>
2022-07-28 19:01:39 +0800

    Initial commit

---
+++ src/main/java/cn/edu/guet/youxuanweapp/YouXuanWeappApplication.java
@@ -0,0 +1,13 @@
+package cn.edu.guet.youxuanweapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YouXuanWeappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YouXuanWeappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cn/edu/guet/youxuanweapp/YouXuanWeappApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.edu.guet.youxuanweapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YouXuanWeappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
